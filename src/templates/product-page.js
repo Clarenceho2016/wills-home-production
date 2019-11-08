a10637db32a572c837f65e70c8b00ef09d0bc741
Clarenceho2016 <info@atlasopen.com>
2019-11-08 16:30:18 +1100

    changing product page

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -43,9 +43,7 @@ export const ProductPageTemplate = ({
       <div className="container">
         <div className="section">
           <div className="columns">
-            <div className="column is-7" style={{
-                justifyContent: 'center !important',
-            }}>
+            <div className="column is-12">
               <h3 className="has-text-weight-semibold is-size-2"  style={{
                 justifyContent: 'center !important',
                 textAlign: 'center',
