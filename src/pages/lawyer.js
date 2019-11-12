543f262f9cb0cb9a6352ce253fa52bebf446ceb2
Clarenceho2016 <info@atlasopen.com>
2019-11-12 17:34:34 +1100

    text changes lawyers page

--- src/pages/lawyer.js
+++ src/pages/lawyer.js
@@ -12,35 +12,18 @@ const lawyer = () => {
                         <div className="content" style={{ padding: '300px', }}>
                             <h1>Lawyers from MyTribe</h1>
                             <p>
-                                This is an example site integrating Netlify’s form handling with
-                                Gatsby
+                                Lawyers can earn money by registering themselves on MyTribe as either a contractor or employee.
                             </p>
                             <ul>
                                 <li>
-                                    <Link to="/contact">Basic contact form</Link>
-                                </li>
-                                <li>
-                                    <Link to="/contact/file-upload/">Form with file upload</Link>
+                                    <Link to="/contact">Lawyers register here</Link>
                                 </li>
                             </ul>
 
-                            <h2>Troubleshooting</h2>
-                            <h3>Forms stop working after upgrading to Gatsby v2</h3>
+                            <h2>What type of Lawyers can register on MyTribe?</h2>
+                            <h3>Lawyers of all types can register.</h3>
                             <p>
-                                This can be caused by the offline-plugin.{' '}
-                                <a href="https://github.com/gatsbyjs/gatsby/issues/7997#issuecomment-419749232">
-                                    Workaround
-                                </a>{' '}
-                                is to use <code>?no-cache=1</code> in the POST url to prevent
-                                the service worker from handling form submissions
-                            </p>
-                            <h3>Adding reCAPTCHA</h3>
-                            <p>
-                                If you are planning to add reCAPTCHA please go to{' '}
-                                <a href="https://github.com/imorente/gatsby-netlify-form-example">
-                                    imorente/gatsby-netlify-form-example
-                                </a>{' '}
-                                for a working example.
+                                On MyTribe, we have a strict application and screening process to ensure high standards of service and delivery.
                             </p>
                         </div>
                     </div>
