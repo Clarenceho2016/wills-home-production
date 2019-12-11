d99cd739d527a70e3a395b9e745c58fd28b7ca94
Clarenceho2016 <info@atlasopen.com>
2019-12-11 11:21:41 +1100

    change

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -67,7 +67,7 @@ export const IndexPageTemplate = ({
                   padding: '0.25em',
                 }}
               >
-                All Day Wills
+                 Day Wills
               </h3>
               <h3
                 className="is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
