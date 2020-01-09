bd33b59eea7fa57d0837ba5e73dc058d8e9ddfbd
Alex <35946230+iceshellwang@users.noreply.github.com>
2020-01-09 11:19:04 +1100

    set as bg img

--- src/pages/referrers.js
+++ src/pages/referrers.js
@@ -1,32 +1,39 @@
 import React from 'react'
-import leftImg from '../img/hs.jpg'
+//import leftImg from '../img/hs.jpg'
 import Layout from '../components/Layout'
 import Link from "gatsby-link";
 import Grid from "@material-ui/core/Grid";
 import Button from "react-bootstrap/Button";
 import { faAlignCenter } from '@fortawesome/free-solid-svg-icons';
-
+import bg from '../../static/img/referees.jpg'
 const referrers = () => {
     return (
         <Layout>
-            <section className="section" >
-
-                <div className="full-width-image-container-lawyer">
-                    <img src={"https://firebasestorage.googleapis.com/v0/b/wills-platform.appspot.com/o/public%2Fimage%2Fphoto-of-people-doing-handshakes-3184420%20(1).jpg?alt=media&token=8688617e-1d81-415b-b5e1-897659383b25"} alt="register" />
-                </div>
-
-                <div className="container" >
-                    <div className="content" style={{ minHeight: "33rem", display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                        <h1 className="has-text-weight-bold is-size-1" style={{ color: 'white', fontSize: '3vw', backgroundColor: '#007bff', padding: '1vw', }}>Referrers from MyTribe</h1>
-                    </div>
-                </div>
-
-            </section>
+            <div
+                className="full-width-image-container margin-top-0"
+                style={{
+                    backgroundImage: `url(${
+                        bg
+                        })`,
+                }}
+            >
+                <h2
+                    className="has-text-weight-bold is-size-1"
+                    style={{
+                        boxShadow: '0.5rem 0 0 #007bff, -0.5rem 0 0 #007bff',
+                        backgroundColor: '#007bff',
+                        color: 'white',
+                        padding: '1rem',
+                    }}
+                >
+                    Referrers from MyTribe
+        </h2>
+            </div>
 
             <section className="section"  >
 
                 <div >
-                    <div style={{ padding: '1vw', display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: "10vw", marginRight: '10vw' }}>
+                    <div style={{ padding: '0.5vw', display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: "10vw", marginRight: '10vw' }}>
 
                         <p style={{ fontSize: '25px', fontWeight: 'bold' }}>What type of organisations can register on MyTribe?
                         </p>
