affaa5eea30b8eda4ca8faa70dc1f0fc705eb513
Alex <alex@atlasopen.com>
2020-02-19 16:01:48 +1100

    general styling-v1

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -13,10 +13,15 @@ class BlogRoll extends React.Component {
         {posts &&
           posts.map(({ node: post }) => (
             <div className="is-parent column is-6" key={post.id}>
+              {/* <article
+                className={`blog-list-item tile is-child box notification ${
+                  post.frontmatter.featuredpost ? 'is-featured' : ''
+                  }`}
+              > */}
               <article
                 className={`blog-list-item tile is-child box notification ${
                   post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
+                  }`}
               >
                 <header>
                   {post.frontmatter.featuredimage ? (
@@ -26,24 +31,28 @@ class BlogRoll extends React.Component {
                           image: post.frontmatter.featuredimage,
                           alt: `featured image thumbnail for post ${
                             post.title
-                          }`,
+                            }`,
                         }}
                       />
                     </div>
                   ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                  </p>
                 </header>
+                <div className="post-meta" style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+                  <Link
+                    className="title has-text-primary is-size-4" style={{ display: 'flex', justifyContent: 'center', textAlign: 'center', }}
+                    to={post.fields.slug}
+                  >
+                    {post.frontmatter.title}
+                  </Link>
+                  <span className="subtitle is-size-5 is-block" style={{ textAlign: 'center', }}>
+                    &bull; {post.frontmatter.date}
+                  </span>
+                  <div >
+
+
+                  </div>
+                </div>
+
                 <p>
                   {post.excerpt}
                   <br />
--- src/pages/referrers.js
+++ src/pages/referrers.js
@@ -5,7 +5,7 @@ import Link from "gatsby-link";
 import Grid from "@material-ui/core/Grid";
 import Button from "react-bootstrap/Button";
 import { faAlignCenter } from '@fortawesome/free-solid-svg-icons';
-import bg from '../../static/img/referees.jpg'
+import bg from '../../static/img/referrer-o.jpg'
 const referrers = () => {
     return (
         <Layout>
@@ -17,14 +17,16 @@ const referrers = () => {
                         })`,
 
                     // backgroundPosition: '10px 18px 18px 20px'
+                    // WebkitMask
+                    //     : '-webkit-linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,1),rgba(0,0,0,1),rgba(0,0,0,0.5))'
                 }}
             >
                 <h2
                     className="has-text-weight-bold is-size-1"
                     style={{
-                        position: 'absolute',
+
                         // marginTop: '19rem',
-                        color: 'white',
+                        color: '#152189',
                         padding: '1rem',
                         marginLeft: '0.05vw'
                     }}
@@ -33,30 +35,7 @@ const referrers = () => {
         </h2>
             </div>
 
-            {/* <section className="section"  >
 
-                <div >
-                    <div style={{ padding: '0.5vw', display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: "10vw", marginRight: '10vw' }}>
-
-                        <p style={{ fontSize: '25px', fontWeight: 'bold' }}>What type of organisations can register on MyTribe?
-                        </p>
-                    </div>
-                  
-
-                </div>
-                <div style={{ marginLeft: "10vw", marginRight: '10vw' }}>
-                    <p>
-                        If you have client’s that need an estate plan, but don’t have the time to meet a lawyer.  Or, perhaps you don’t have a trusted lawyer you can refer your valued clients to.  MyTribe allows your organisation to create an account and make bookings on behalf of your clients. One of our lawyers can come to your office or to your client’s home, enabling you to be as involved in the process as you like and ensuring that your client’s get expert advice and an estate plan.
-                        </p>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '20px' }}>
-                    <ul>
-                        <li style={{ color: 'white' }}>
-                            <a href="https://app.my-tribe.com.au/referrer-signup">Referrers register here</a>
-                        </li>
-                    </ul>
-                </div>
-            </section> */}
             <section className="section" style={{ marginTop: '0.5rem', marginBottom: '0.8rem', }}>
                 <h1 style={{ fontSize: '3rem', fontWeight: 'bold', display: 'flex', alignItems: 'center', justifyContent: 'center', marginBottom: '0.75em' }}>For referrers</h1>
                 <div style={{ display: 'flex' }}>
@@ -64,19 +43,47 @@ const referrers = () => {
                         <img src='https://firebasestorage.googleapis.com/v0/b/wills-platform.appspot.com/o/public%2Fimage%2Freferrer.jpg?alt=media&token=8c9907c6-681b-4db4-bc9b-7b17a2c405de' alt="register" ></img>
                     </Grid>
                     <Grid item xs={12} sm={6} >
-                        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '3rem' }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingLeft: '3rem', paddingRight: '3rem' }}>
 
-                            <p style={{ fontSize: '1.5rem', }}> If you have client’s that need an estate plan, but don’t have the time to meet a lawyer.  Or, perhaps you don’t have a trusted lawyer you can refer your valued clients to.  MyTribe allows your organisation to create an account and make bookings on behalf of your clients. </p></div>
+                            <h2 style={{ lineHeight: '1.5em', fontSize: '1.25rem', fontWeight: 'bold' }}> If you have client’s that need an estate plan, but don’t have the time to meet a lawyer.<br /><br /></h2><h3 style={{ lineHeight: '1.5em', }}>  Or, perhaps you don’t have a trusted lawyer you can refer your valued clients to.  MyTribe allows your organisation to create an account and make bookings on behalf of your clients.
+                            </h3></div>
+
+
+                        <div
+                            style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '1rem', paddingTop: '0.5vw' }}>
+
+
+                            <Button variant="primary" href='https://app.my-tribe.com.au/referrer-signup' style={{
+                                margin: '10px',
+                                // paddingLeft: '0.75em',
+                                // paddingRight: '0.75em',
+                                // paddingTop: '0.5em',
+                                // paddingBottom: '0.5em',
+                                color: 'white',
+                                fontSize: '1em',
+                                borderColor: '#FFF',
+                                borderRadius: '10px',
+
+                                // width: '80px',
+                                // height: '37px',
+                                lineHeight: '1.5em',
+                                backgroundColor: '#007bff',
+                            }}>
+                                Register
+
+              </Button>
+                        </div>
                     </Grid>
                 </div>
-                <div
+
+                {/* <div
                     style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '1rem', padding: '0.5vw' }}>
                     <ul>
                         <li style={{ color: 'white' }}>
                             <a href="https://app.my-tribe.com.au/referrer-signup" style={{ display: 'flex', ali7gnItems: 'center', justifyContent: 'center', fontSize: '32px' }}>Referrers register here</a>
                         </li>
                     </ul>
-                </div>
+                </div> */}
 
 
 
